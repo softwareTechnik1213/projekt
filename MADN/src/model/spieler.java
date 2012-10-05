329715a7a5bcd5ad21d33a4ae9366fc388990011
chgeiger <chgeiger@lenny.in.fhkn.de>
2012-10-05 16:44:53 +0200

    Spieler Attribute hinzugefuegt

--- MADN/src/model/spieler.java
+++ MADN/src/model/spieler.java
@@ -1,7 +1,30 @@
 package model;
 
+import java.awt.Color;
+
 public class spieler {
 
+	private int id;
+	private Color color;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public static void main(String[] args) {
 		
 	}
