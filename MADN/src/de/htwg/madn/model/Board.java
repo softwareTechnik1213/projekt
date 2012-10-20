7f4bfd82c6f5aca64a75cd7c350d467bc00b062f
Simon Kessler <simon.kessler@me.com>
2012-10-20 23:31:25 +0200

    started to implement TUI. Lots to do in Bord

--- MADN/src/de/htwg/madn/model/Board.java
+++ MADN/src/de/htwg/madn/model/Board.java
@@ -11,7 +11,7 @@ public final class Board {
     private PublicContainer pc;
     private final int players = 4;
     private final int figuresPerPlayer = 4;
-    private final int publicFields = 28;
+    private final int publicFields = 40;
 
     public Board() {
         // create all home and finish field containers
@@ -25,17 +25,20 @@ public final class Board {
 
     @Override
     public String toString() {
-        char[][] field = new char[21][21];
+        int rows = 11;
+        int cols = 11;
 
-        for (int i = 0; i < 21; i++) {
-            for (int j = 0; j < field.length; j++) {
+        char[][] field = new char[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 field[i][j] = getFieldChar(i, j);
             }
         }
 
-        StringBuilder sb = new StringBuilder(field.length * field.length);
-        for (int i = 0; i < field.length; i++) {
-            for (int j = 0; j < field.length; j++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 sb.append(field[i][j]);
             }
             // add a line break after each line
@@ -53,21 +56,22 @@ public final class Board {
 
         /* plan:
 
-            -------21x21---------
-              --5--       --5--
-                    --5--
+            ---11x11---
 
-            **      + + +      **
-            **      + # +      **
-                    + # +
-                    + # +
-            + + + + + # + + + + +
-            + # # # #   # # # # +
-            + + + + + # + + + + +
-                    + # +
-                    + # +
-            **      + # +      **
-            **      + + +      **
+            **  +++  **
+            **  +#+  **
+                +#+
+                +#+
+            +++++#+++++
+            +#### ####+
+            +++++#+++++
+                +#+
+                +#+
+            **  +#+  **
+            **  +++  **
+
+            40 public fields!
+
 
             where the top left is start.
             then it goes right and down.
@@ -77,13 +81,15 @@ public final class Board {
                   |           V
                   <------------
 
+            This direction applies to all field containers as well
+
 
          */
 
         // get cell type
         if (!isPlayableField(row, col)) {
             ret = spaceField;
-        } else if (isSetField(row, col)) {
+        } else if (isOccupiedField(row, col)) {
             ret = 'X'; // should be a b c d e f.... according to figure no
         } else if (isHomeField(row, col)) {
             ret = homeField;
@@ -97,16 +103,20 @@ public final class Board {
     }
 
     private boolean isHomeField(int row, int col) {
-        return (row == 0 || row == 1 || row == 19 || row == 20)
-                && (col == 0 || col == 1 || col == 19 || col == 20);
+        return (row == 0 || row == 1 || row == 9 || row == 10)
+                && (col == 0 || col == 1 || col == 9 || col == 10);
     }
 
     private boolean isPublicField(int row, int col) {
-        return true;
+        return ((col == 4 || col == 6) && row != 5)
+                || ((row == 4 || row == 6) && col != 5)
+                || (col == 5 && (row == 0 || row == 10))
+                || (row == 5 && (col == 0 || col == 10));
     }
 
     private boolean isFinishField(int row, int col) {
-        return true;
+        return (col == 5 && row >= 1 && row <= 9 && row != 5)
+                || (row == 5 && col >= 1 && col <= 9 && col != 5);
     }
 
     private boolean isPlayableField(int row, int col) {
@@ -114,11 +124,233 @@ public final class Board {
                 || isFinishField(row, col);
     }
 
-    private boolean isSetField(int row, int col) {
+    private boolean isOccupiedField(int row, int col) {
         if (!isPlayableField(row, col)) {
             return false;
         }
 
-        return true;
+        // check systematically all fields if someone is on it
+
+        if (isHomeField(row, col)) {
+            return checkOccupiedHomeField(row, col);
+        } else if (isFinishField(row, col)) {
+            return checkOccupiedFinishField(row, col);
+        } else if (isPublicField(row, col)) {
+            return checkOccupiedPublicField(row, col);
+        } else {
+            throw new IllegalStateException("this shouldn't happen");
+        }
+    }
+
+    // this method expects that row, col refers to a home field!
+    private boolean checkOccupiedHomeField(int row, int col) {
+        // go to the correct home container
+
+        /*  home container numeration
+
+         0          1
+
+
+         3          2
+
+
+         */
+        int targetContainer;
+        if (col >= 0 && col <= 1 && row >= 0 && row <= 1) {
+            targetContainer = 0;
+        } else if (col >= 9 && col <= 10 && row >= 0 && row <= 1) {
+            targetContainer = 1;
+        } else if (col >= 9 && col <= 10 && row >= 9 && row <= 10) {
+            targetContainer = 2;
+        } else {
+            targetContainer = 3;
+        }
+
+        int i = 0;
+        HomeContainer p = null;
+        for (HomeContainer hc : hclist) {
+            p = hc;
+            if (i == targetContainer) {
+                break;
+            }
+            i++;
+        }
+        if (p == null) {
+            throw new IllegalStateException();
+        }
+
+        // now get the right field in p (which points to the right home container)
+        /*
+            0 1
+            3 2
+        */
+        int targetField;
+
+        // top row of each container
+        if (row == 0 || row == 9) {
+            // first one or second one?
+            if (col == 0 || col == 9) {
+                targetField = 0;
+            } else {
+                targetField = 1;
+            }
+        } else { // bottom row of each container
+            // first one or second one?
+            if (col == 0 || col == 9) {
+                targetField = 3;
+            } else {
+                targetField = 2;
+            }
+        }
+
+        int j = 0;
+        Field q = null;
+        for (Field f : p.fieldList()) {
+            q = f;
+            if (j == targetField) {
+                break;
+            }
+            j++;
+        }
+        if (q == null) {
+            throw new IllegalStateException();
+        }
+
+        return q.getOccupier() != null;
+    }
+
+    // this method expects that row, col refers to a finish field!
+    private boolean checkOccupiedFinishField(int row, int col) {
+
+        /*
+          finish field numeration
+
+           0
+
+           #
+           #
+           #
+           #
+     3 #### #### 1
+           #
+           #
+           #
+           #
+
+           2
+
+         */
+
+        // get target container
+        int targetContainer;
+
+        if (col == 5 && row <= 4) {
+            targetContainer = 0;
+        } else if (row == 5 && col >= 6) {
+            targetContainer = 1;
+        } else if (row == 5 && col <= 4) {
+            targetContainer = 2;
+        } else {
+            targetContainer = 3;
+        }
+
+        int i = 0;
+        FinishContainer p = null;
+        for (FinishContainer fc : fclist) {
+            p = fc;
+            if (i == targetContainer) {
+                break;
+            }
+            i++;
+        }
+        if (p == null) {
+            throw new IllegalStateException();
+        }
+
+        // now get the correct field
+
+        /*
+              #0
+              #1
+              #2
+              #3
+          0123 3210
+          #### ####
+              #3
+              #2
+              #1
+              #0
+         */
+
+        int targetField;
+
+        // calculate the margin from the edges
+        if (col == 5 && row <= 4) { // top container
+            targetField = row - 1;
+        } else if (col == 5 && row >= 6) { // bottom container
+            targetField = 10 - row - 1;
+        } else if (row == 5 && col <= 4) { // left container
+            targetField = col - 1;
+        }  else { // right container
+            targetField = 10 - col - 1;
+        }
+
+        int j = 0;
+        Field q = null;
+        for (Field f : p.fieldList()) {
+            q = f;
+            if (j == targetField) {
+                break;
+            }
+            j++;
+        }
+        if (q == null) {
+            throw new IllegalStateException();
+        }
+
+        return q.getOccupier() != null;
+    }
+
+    // this method expects that row, col refers to a public field!
+    private boolean checkOccupiedPublicField(int row, int col) {
+
+        /*
+           field numeration:
+                Start point
+                |
+                V
+            **  012  **
+            **  +#3  **
+                +#4
+                +#5
+            +++++#6789+  and so on
+            +#### ####+
+            +++++#+++++ <-- 12
+                +#+
+                +#+
+            **  +#+  **
+            **  +++  **
+
+
+         */
+
+        // get target field
+        int targetField;
+
+        if (row == 0) { // top row
+            targetField = row - 4;
+        } else if (col == 6 && row <= 4) { // top right col including both corners
+            targetField = 2 + row;
+        } else if (row == 4 && col >= 7) { // right top row including right corner
+
+        }
+
+
+
+
+
+
+
+        return false;
     }
 }
