c77d18ad7aa94dc6075adf8f13c6b1eafc31557d
sikessle <simon.kessler@me.com>
2012-11-02 13:54:56 +0100

    added sonar project file (not working)

--- MADN/test/de/htwg/madn/model/PublicContainerTest.java
+++ MADN/test/de/htwg/madn/model/PublicContainerTest.java
@@ -1,24 +1,13 @@
 package de.htwg.madn.model;
 
-import java.awt.Color;
 import static org.junit.Assert.assertSame;
-import org.junit.Before;
 import org.junit.Test;
 
 public class PublicContainerTest {
 
-    private Player p1;
-    private PublicContainer pc;
-
-    @Before
-    public void setUp() throws Exception {
-        p1 = new Player(1, Color.red, "p1");
-
-    }
-
     @Test
     public void testPublicContainer() {
-        pc = new PublicContainer(4);
+        new PublicContainer(4);
     }
 
     @Test
@@ -33,9 +22,9 @@ public class PublicContainerTest {
     @Test
     public void testToCharArray() {
         PublicContainer f = new PublicContainer(40);
-        char[][] ca = f.toCharArray();
+        f.toCharArray();
 
         f = new PublicContainer(20);
-        char[][] ce = f.toCharArray();
+        f.toCharArray();
     }
 }
