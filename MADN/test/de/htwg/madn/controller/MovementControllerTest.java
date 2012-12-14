66b45a0ea8376ab5432f15509f92a5dba3c2bed3
Simon Kessler <simon.kessler@me.com>
2012-12-14 18:52:41 +0100

    tests

--- MADN/test/de/htwg/madn/controller/MovementControllerTest.java
+++ MADN/test/de/htwg/madn/controller/MovementControllerTest.java
@@ -1,30 +1,46 @@
 package de.htwg.madn.controller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import de.htwg.madn.controller.MovementController.AutonomeMovementController;
+import de.htwg.madn.model.Board;
 import de.htwg.madn.model.Dice;
+import de.htwg.madn.model.GameSettings;
 import de.htwg.madn.model.IGameSettings;
 import de.htwg.madn.model.IModelPort;
+import de.htwg.madn.model.ModelPort;
 
 public class MovementControllerTest {
 
-	private final Dice dice;
-	private final IGameSettings settings;
-	private String status;
-	private final IModelPort model;
-	public MovementController 
+	private Dice dice;
+	private IGameSettings settings;
+	private IModelPort model;
+	private BoardController boardController;
+
+	private static final int MINPLAYERS = 1;
+	private static final int MAXPLAYERS = 2;
+	private static final int FIGURESPERPLAYER = 1;
+	private static final int PUBLICFIELDSCOUNT = 6;
+	private static final int DICEMIN = 6;
+	private static final int DICEMAX = 6;
+	private static final int MINNUMBERTOEXITHOME = 1;
+	private static final int THROWSALLOWEDINHOME = 3;
+	private static final int THROWSALLOWEDINPUBLIC = 1;
+
 	@Before
 	public void setUp() throws Exception {
 
-			this.model = model;
-			this.dice = model.getDice();
-			this.settings = model.getSettings();
-			this.status = "";
-			this.autonomeComtroller = new AutonomeMovementController();
+		settings = new GameSettings(MINPLAYERS, MAXPLAYERS, FIGURESPERPLAYER,
+				PUBLICFIELDSCOUNT, DICEMIN, DICEMAX, MINNUMBERTOEXITHOME,
+				THROWSALLOWEDINHOME, THROWSALLOWEDINPUBLIC);
+		model = new ModelPort(settings, new Board(settings));
+		
+		boardController = new BoardController(model);
+	
+		this.dice = model.getDice();
+		this.settings = model.getSettings();
 	}
 
 	@Test
