c316878a2beeba8f54f8326ab72229d1243c9dc1
sikessle <simon.kessler@me.com>
2013-01-18 14:24:23 +0100

    commit

--- MADN/src/de/htwg/madn/Application.java
+++ MADN/src/de/htwg/madn/Application.java
@@ -18,7 +18,7 @@ public final class Application {
 	
 	private static final int MINPLAYERS = 1;
 	private static final int MAXPLAYERS = 2;
-	private static final int FIGURESPERPLAYER = 4;
+	private static final int FIGURESPERPLAYER = 2;
 	private static final int PUBLICFIELDSCOUNT = 20;
 	private static final int DICEMIN = 1;
 	private static final int DICEMAX = 6;
